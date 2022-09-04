0a150e7757e4d85aee1696a3d05443e970a78c75
“sanny085” <sannykumar085@gmail.com>
2022-09-05 00:36:42 +0530

    updated services name

--- policyStore.py
+++ policyStore.py
@@ -6,7 +6,7 @@ def valid_user_for_policy(name, age, mobile):
     print('Namaste,', name, 'You are eligible to buy policy')
     if age > 45:
         high_risk_policy = ['SBI Lite', 'Pnb MetLife']
-        print('Please choose any policy', high_risk_policy[0])
+        print('Please choose any policy', high_risk_policy[0], high_risk_policy[0])
     else:
         print('Purchase HDFC plan')
 
