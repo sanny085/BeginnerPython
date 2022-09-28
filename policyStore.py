af91c454079af8da3b3276775b5afda2f4c304d3
sanny085 <sannykumar085@gmail.com>
2022-09-28 17:14:58 +0530

    change method flow

--- policyStore.py
+++ policyStore.py
@@ -1,26 +1,6 @@
 name = str(input('Enter your Name : '))
 age = int(input('Enter your Age : '))
 
-def policyPlanDetails(user_policy):
-    hr_policy_details = [{
-        'name': 'SBI Lite',
-        'company': 'LIC',
-        'premium': 3000,
-        'isReqMedical': True,
-       },
-       {
-            'name': 'Pnb MetLife',
-            'company': 'Punjab National Bank',
-            'premium': 2800,
-            'isReqMedical': True,
-        }
-    ]
-    for i in range(len(hr_policy_details)):
-        if hr_policy_details[i]['name'] == user_policy:
-            print(hr_policy_details[i])
-
-
-
 def valid_user_for_policy(name, age, mobile):
     print('Namaste,', name, 'You are eligible to buy policy')
     if age > 45:
@@ -38,6 +18,25 @@ def valid_user_for_policy(name, age, mobile):
             return
     else:
         print('Purchase HDFC plan')
+
+
+def policyPlanDetails(user_policy):
+    hr_policy_details = [{
+        'name': 'SBI Lite',
+        'company': 'LIC',
+        'premium': 3000,
+        'isReqMedical': True,
+       },
+       {
+            'name': 'Pnb MetLife',
+            'company': 'Punjab National Bank',
+            'premium': 2800,
+            'isReqMedical': True,
+        }
+    ]
+    for i in range(len(hr_policy_details)):
+        if hr_policy_details[i]['name'] == user_policy:
+            print(hr_policy_details[i])
 
 
 if age > 18:
